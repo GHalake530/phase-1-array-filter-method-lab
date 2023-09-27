52cd8223ff844b35aa695075078b7dd93d0da298
Guyo <guyo.halake@students.moringaschool.com>
2023-09-27 12:18:01 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+// function that returns drivers name matching
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  // Function that find drivers whose name start with the letters they want
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(letters.toLowerCase()));
+  }
+  // check if name matches
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  // testing
+  module.exports = {
+    findMatching,
+    fuzzyMatch,
+    matchName,
+  };
+  
